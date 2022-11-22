947f11791264d9dd723f9ef2277e85bfc4faf6f5
Sergey Neskin <neskin.x@mail.ru>
2022-11-22 21:58:58 +0700

    added ts(typescript) for button

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -5,20 +5,32 @@ import {
 	ButtonSpinner,
 } from './button.styles.jsx';
 
-export const BUTTON_TYPE_CLASSES = {
-	base: 'base',
-	google: 'google-sign-in',
-	inverted: 'inverted',
-};
+export enum BUTTON_TYPE_CLASSES {
+	base,
+	google,
+	inverted,
+}
 
-const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
+interface IButton extends React.HTMLAttributes<HTMLButtonElement> {
+	buttonType: BUTTON_TYPE_CLASSES;
+	isLoading: boolean;
+}
+
+const getButton = (
+	buttonType: BUTTON_TYPE_CLASSES = BUTTON_TYPE_CLASSES.base
+) =>
 	({
 		[BUTTON_TYPE_CLASSES.base]: BaseButton,
 		[BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
 		[BUTTON_TYPE_CLASSES.inverted]: invertedButton,
 	}[buttonType]);
 
-const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
+const Button = ({
+	children,
+	buttonType,
+	isLoading,
+	...otherProps
+}: IButton) => {
 	const CustomButton = getButton(buttonType);
 	return (
 		<CustomButton disabled={isLoading} {...otherProps}>
